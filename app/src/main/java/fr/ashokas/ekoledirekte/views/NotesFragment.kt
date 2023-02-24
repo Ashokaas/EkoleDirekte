e22dd89c7427e2596619d5bef6a9c7b650405f2f
Ashokas <antotosun@gmail.com>
2023-02-25 00:14:04 +0100

    Début affichage des notes dans le fragment NotesFragment.kt

--- app/src/main/java/fr/ashokas/ekoledirekte/views/NotesFragment.kt
+++ app/src/main/java/fr/ashokas/ekoledirekte/views/NotesFragment.kt
@@ -4,14 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.Fragment
 import fr.ashokas.ekoledirekte.R
 import fr.ashokas.ekoledirekte.api.AccountData
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
+import org.json.JSONArray
+import org.json.JSONObject
 
 class NotesFragment : Fragment() {
 
@@ -39,14 +41,91 @@ class NotesFragment : Fragment() {
         val id = bundle?.getString("id")
         val photo_url = bundle?.getString("photo_url")
 
-        val test = frag?.findViewById<TextView>(R.id.test_view)
-        if (test != null) {
-            test.text = prenom
-        }
-
         println(moy)
         println(nom)
         println(id)
+
+        CoroutineScope(Dispatchers.IO).launch {
+            val notes = CoroutineScope(Dispatchers.IO).async {
+                return@async AccountData.getNotes(
+                    token = token.toString(),
+                    id = id.toString()
+                )
+            }.await()
+
+            val notesJArray = notes["notes"] as JSONArray
+
+            var grades = listOf(
+                mutableMapOf<String, MutableList<List<String>>>(),
+                mutableMapOf<String, MutableList<List<String>>>(),
+                mutableMapOf<String, MutableList<List<String>>>()
+            )
+
+
+            for (i in 0 until notesJArray.length()) {
+                val note: JSONObject = notesJArray.get(i) as JSONObject
+
+                val trimestreNote = note["codePeriode"].toString().get(3).digitToInt() - 1
+
+                println(trimestreNote)
+
+                val matiereNote = note["libelleMatiere"]
+                val devoirNote = note["devoir"]
+                val valeurNote = note["valeur"]
+                val surcmbNote = note["noteSur"]
+                if (matiereNote !in grades[trimestreNote]) {
+                    grades[trimestreNote][matiereNote as String] = mutableListOf()
+                }
+
+                grades[trimestreNote][matiereNote]!!.add(listOf(devoirNote, valeurNote, surcmbNote) as List<String>)
+            }
+
+            println(grades)
+
+
+
+            for (trim in 0 until 3) {
+                for (mat in grades[trim].keys) {
+                    println(grades[trim][mat])
+                    afficher_notes(view, savedInstanceState, mat + (grades[trim][mat]).toString())
+
+                }
+            }
+        }
+
+
+    }
+
+
+    fun afficher_notes(view: View, savedInstanceState: Bundle?, text: String) {
+        GlobalScope.launch(Dispatchers.Main) {
+            val matiere = TextView(requireContext())
+            matiere.textSize = 18f
+            matiere.text = text
+            matiere.typeface = ResourcesCompat.getFont(requireContext(), R.font.poppins_light)
+            matiere.layoutParams = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+            )
+            matiere.setPadding(16, 8, 0, 8)
+
+            matiere.setTextColor(
+                ContextCompat.getColor(
+                    requireContext(),
+                    R.color.black
+                )
+            ) // définir la couleur du texte
+            matiere.setBackgroundColor(
+                ContextCompat.getColor(
+                    requireContext(),
+                    R.color.grey
+                )
+            ) // définir la couleur de fond
+
+
+            val parentView = view.findViewById<LinearLayout>(R.id.grades_table)
+            parentView.addView(matiere)
+        }
     }
 
 }
